fe06ccc6835a09b8a8d88ea12ffb6f38cb257288
slightlycyborg <collinalexbell@gmail.com>
2017-05-27 19:23:00 -0400

    Added collect-dishes and funcs it depends on

--- src/room_cleaning_algo/core.clj
+++ src/room_cleaning_algo/core.clj
@@ -1,6 +1,26 @@
 (ns room-cleaning-algo.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-input []
+  (read-line))
+
+(defn put-output [output]
+  (println output))
+
+(defn handle-found-dishes []
+  (println "Take it(them) to the sink or dishwasher. Waiting for input to continue.")
+  ;;The depth of the stack would be enormous if the command was translated all the way down to base units (muscle movements?).
+  (println "If you put them in the dishwasher, make sure they are rinsed first")
+  (println "If you see dishes on the way out, pick them up"))
+
+(defn collect-dishes
+  []
+  (loop [trips-made-to-kitchen 0]
+    (println "Scan the room. Do you see a dishes? Y/N")
+    (let [input (get-input)]
+      (if (or (= input "y") (= input "Y"))
+        (do (handle-found-dishes)
+            (get-input)
+            (recur (+ 1 trips-made-to-kitchen)))
+        (do
+          (println "Congrats, you are done doing dishes")
+          trips-made-to-kitchen)))))
